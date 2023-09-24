a1dc8f478b185a3739081fe1b7ae088f1cb49777
LUCAS LIAO <lucas@11a0.com>
2023-09-24 12:04:02 +0800

    added rate limiter

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const rateLimit = require('express-rate-limit');
 const userRoutes = require('./routes/userRoute');
 const eventRoutes = require('./routes/eventRoute');
 
 // Middleware to parse JSON request bodies
 app.use(bodyParser.json());
+
+
+// Define the rate limiter
+const apiLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,  // 15 minutes
+  max: 100,                  // Limit each IP to 100 requests per windowMs
+  message: 'Too many requests from this IP, please try again after 15 minutes'
+});
+
+// Apply the rate limiter to all routes
+app.use(apiLimiter);
 
 // Define routes
 app.use('/api', userRoutes); 
